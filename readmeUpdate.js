5ec289418ccdfeeb3566eab82c739cd7bc035a19
hellonaeunkim <hellonaeunkim@gmail.com>
2025-02-02 23:58:50 +0900

    initial setting

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -70,8 +70,7 @@ let text = `
 
 <div align= "center">
     <h2 style="border-bottom: 1px solid #d8dee4; color: #282d33;"> 📕 Latest Blog Posts </h2>
-    <div align="center" style="display: flex; flex-direction: column; align-items: center; gap: 6px; margin: 20px 0;">
-    </div>
+    <div style="text-align: center; max-width: 800px; margin: 20px auto; list-style-position: inside;">
 </div>
 
 
@@ -94,7 +93,7 @@ const parser = new Parser({
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
-        text += `<a href="${link}" style="text-decoration: none; color: #0366d6; font-size: 14px;">${title}</a>\n`;
+        text += `• <a href="${link}" style="text-decoration: none; color: #0366d6; display: inline-block;">${title}</a>\n`;
     }
 
     text += '</div></div>\n';
